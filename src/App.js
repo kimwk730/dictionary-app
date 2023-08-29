4957f7d7a5cec92f2a0060ebf28f6acfb279ea09
kimwk730 <kimwk730@users.noreply.github.com>
2023-08-28 14:31:43 -1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App container">
+			<a href="/" className="btn btn-primary">
+				Hello
+			</a>
+		</div>
+	);
+}
+
+export default App;
