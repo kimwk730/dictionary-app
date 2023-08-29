e0f10bbc2ff2ce6650fd8e6b78374aebc6705452
kimwk730 <kimwk730@users.noreply.github.com>
2023-08-29 12:42:15 -1000

    phonetics added

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Meaning(props) {
-	console.log(props.meaning);
 	return (
 		<div>
 			<h3>{props.meaning.partOfSpeech}</h3>
--- src/Result.js
+++ src/Result.js
@@ -3,9 +3,26 @@ import Meaning from "./Meaning";
 
 export default function Result(props) {
 	if (props.apiResult) {
+		console.log(props.apiResult.phonetics);
 		return (
 			<div className="result">
 				<h2>{props.apiResult.word}</h2>
+				{props.apiResult.phonetics.map(function (phonetic, index) {
+					return (
+						<div key={index}>
+							<div className="phonetic">
+								<a
+									href={props.apiResult.phonetics[0].audio}
+									target="_blank"
+									rel="noreferrer"
+								>
+									Listen
+								</a>
+								{props.apiResult.phonetics[0].text}
+							</div>
+						</div>
+					);
+				})}
 				{props.apiResult.meanings.map(function (meaning, index) {
 					return (
 						<div key={index}>
